c3f6706fb47395168cc903ae1a3243b3926f7373
Ryan Whitman <whitmanr@us.ibm.com>
2021-04-01 17:47:20 -0400

    Config Update

--- nvim/lua/ryan/lsp.lua
+++ nvim/lua/ryan/lsp.lua
@@ -5,5 +5,7 @@ local on_attach = require'completion'.on_attach
 require'lspconfig'.gopls.setup { on_attach=on_attach }
 require'lspconfig'.pyright.setup { on_attach=on_attach }
 require'lspconfig'.dockerls.setup { on_attach=on_attach }
+require'lspconfig'.dartls.setup { on_attach=on_attach }
+require'lspconfig'.rust_analyzer.setup { on_attach=on_attach }
 
 vim.api.nvim_command([[autocmd BufWritePre *.go lua vim.lsp.buf.formatting_sync()]])
